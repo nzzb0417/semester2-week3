1e4f3a80b13a066b2ed587716ee0f3daadefb56d
nzzb0417 <nzzb0417@leeds.ac.uk>
2026-08-07 00:45:27 +0100

    Update salary.c

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -3,11 +3,11 @@
 
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name:Abdimalik Hussein
+ * ID:202028878
  */
 
- int main( void ) {
+int main(void) {
 
     // define and initialise variables for the problem data
     float salary = 36250.0;
@@ -43,16 +43,3 @@
 
     return 0;
 }
-
-    // define and initialise variables for the problem data 
-
-    // calculate the deductions and final take-home salary
-
-    // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    //printf("NI contribution £...",var_name);
-    //printf("Tax contribution £...",var_name);
-    //printf("Take home salary £...",var_name);
-
-    return 0;
- }
